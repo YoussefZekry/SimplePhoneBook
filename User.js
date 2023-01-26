effa97a8915f7fab5e2eb88469be407035179604
youssef <youssefzekry97@gmail.com>
2023-01-27 00:14:01 +0200

    changed everything

--- User.js
+++ User.js
@@ -1,57 +1,124 @@
 class User {
-    #name;
-    #phone;
-    #email;
-    #gender;
+  #name;
+  #phone;
+  #email;
+  #gender;
 
-    constructor(name, phone, email, gender) {
-      this.#name = name;
-      this.#phone = phone;
-      this.#email = email;
-      this.#gender = gender;
-    }
-    setName(name) {
-      this.#name = name;
-    }
-    setPhone(phone) {
-      this.#phone = phone;
-    }
-    setEmail(email) {
-      this.#email = email;
-    }
-    setGender(gender) {
-      this.#gender = gender;
-    }
-    getName() {
-      return this.#name;
-    }
-    getEmail() {
-      return this.#email;
-    }
-    getGender() {
-      return this.#gender;
-    }
-    getPhone() {
-      return this.#phone;
+  constructor(name, phone, email, gender) {
+    this.#name = name;
+    this.#phone = phone;
+    this.#email = email;
+    this.#gender = gender;
+  }
+  setName(name) {
+    this.#name = name;
+  }
+  setPhone(phone) {
+    this.#phone = phone;
+  }
+  setEmail(email) {
+    this.#email = email;
+  }
+  setGender(gender) {
+    this.#gender = gender;
+  }
+  getName() {
+    return this.#name;
+  }
+  getEmail() {
+    return this.#email;
+  }
+  getGender() {
+    return this.#gender;
+  }
+  getPhone() {
+    return this.#phone;
+  }
+}
+
+var users = [
+  new User("Youssef Zekry", "01060665412", "youssef.com", "male"),
+  new User("Mahmoud Fath", "0106533455", "Fath.com", "male"),
+  new User("Michael Tarek", "0103521531", "Michael.com", "male"),
+  new User("Mona Mahrous", "01035156189", "Mona.com", "female"),
+];
+
+var newUsers = [];
+var index;
+var femaleImage = "resources/img/female.png";
+var maleImage = "resources/img/male.png";
+//------------------------------------------sorting list------------------------------------------
+function getList() {
+  users = users.sort((a, b) => (a.getName() > b.getName() ? 1 : -1));
+  var result = "";
+  for (let i = 0; i < users.length; i++) {
+    result += "<li onclick='view(" + i + ")'><a href='#contactInfo'>";
+
+    if (users[i].getGender().toUpperCase() == "MALE")
+      result += '<img src="resources/img/male.png">';
+    else result += '<img src="resources/img/female.png">';
+
+    result += `<h2>${users[i].getName()}</h2></a>`;
+    result += `<a href="tel:${users[i].getPhone}" data-rel="popup" data-position-to="window" data-transition="pop" data-icon="phone"></a>`;
+    result += "</li> ";
+  }
+  return result;
+}
+
+// ------------------------------------------show profile data ------------------------------------------
+function view(i) {
+  index = i;
+  if (users[i].getGender().toUpperCase() == "MALE") {
+    $("#profileImage").attr("src", maleImage);
+  } else {
+    $("#profileImage").attr("src", femaleImage);
+  }
+  $("#name").val(users[i].getName());
+  $("#phone").val(users[i].getPhone());
+  $("#email").val(users[i].getEmail());
+  $("#gender").val(users[i].getGender());
+}
+
+// ------------------------------------------Creating new array after deleting a specific contact------------------------------------------
+function deleteArray(index1) {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i] == users[index1]) {
+    } else {
+      newUsers.push(users[i]);
     }
   }
 
-  var users = [];
-  function getList() {
-    users = users.sort((a,b)=>a.getName() > b.getName()? 1:-1);
-    var result = "";
-    for (let i = 0; i < users.length; i++) {
-    
-      result +=' <li><a href="#contactInfo">';
+  users = newUsers;
+}
+//------------------------------------------deleting contact------------------------------------------
+function deleteUser() {
+  var name = users[index].name;
 
-      if(users[i].getGender().toUpperCase() ==="MALE")
-      result +='<img src="resources/img/male.png">';
-      else
-      result +='<img src="resources/img/female.png">';
+  deleteArray(index);
+  showList();
+  $("ul").listview("refresh");
+  window.location.href = "#contactList";
+}
 
-      result +=`<h2>${users[i].getName()}</h2></a>`;
-      result +=`<a href="tel:${users[i].getPhone}" data-rel="popup" data-position-to="window" data-transition="pop" data-icon="phone"></a>`;
-      result +="</li> ";
-    }
-    return result;
-  }
+//------------------------------------------ viewing data for edit profile ------------------------------------------
+function editView() {
+  $("#editName").val(users[index].getName());
+  $("#editPhone").val(users[index].getPhone());
+  $("#editEmail").val(users[index].getEmail());
+}
+//------------------------------------------editing contact------------------------------------------
+function edit() {
+  editName = users[index].name = $("#editName").val();
+  editPhone = users[index].phone = $("#editPhone").val();
+  editEmail = users[index].email = $("#editEmail").val();
+  
+  $("#name").val(users[index].setName(editName));
+  $("#phone").val(users[index].setPhone(editPhone));
+  $("#email").val(users[index].setEmail(editEmail));
+  showList();
+  $("ul").listview("refresh");
+  console.log(users[index].name);
+  console.log(users[index].phone);
+  console.log(users[index].email);
+  view(index);
+}
--- object.js
+++ object.js
@@ -1,26 +1,31 @@
-function showList(){
-    $('ul').html(getList());
+function showList() {
+  $("ul").html(getList());
+  $("ul").listview("refresh");
 }
 $(document).ready(function () {
-    showList();
+  showList();
 
-//on click save button 
-$("#split").click (function(){
-    
-    var name = document.getElementById("nameInput")
-    var number = document.getElementById("numberInput")
-    var email = document.getElementById("emailInput")
-    var gender = document.getElementById("slider-flip-m")
-
-    var obj = new User(name.value, number.value,email.value, gender.value )
-    users.push(obj);
-    console.log(gender.value);
-    showList();
-    $('ul').listview('refresh');
-    window.location.href = '#contactList';
-})
-console.log(users)
-
-}
-
-);
+  //------------------------------------------onclick save button ------------------------------------------
+  $("#save").click(function () {
+    var name = document.getElementById("nameInput");
+    var number = document.getElementById("numberInput");
+    var email = document.getElementById("emailInput");
+    var gender = document.getElementById("slider-flip-m");
+    if (!(name.value && number.value)) {
+      alert("Add info to save as contact.");
+      window.location.href = "#newContact";
+    } else {
+      var obj = new User(name.value, number.value, email.value, gender.value);
+      users.push(obj);
+      showList();
+      $("ul").listview("refresh");
+      window.location.href = "#contactList";
+      $("#myForm")[0].reset();
+    }
+  });
+});
+//------------------------------------------onclick cancel------------------------------------------
+$("#cancel").click(function () {
+  console.log("canceled");
+  $("#myForm")[0].reset();
+});
